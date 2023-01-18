8a4b74544da8d7d32cd7b71ac8c600d3e2810ba7
LYF <lyf>
2023-01-18 23:44:34 +0800

    写了个查询所有加入的shops

--- src/main/java/com/example/drama_kill_system/service/IUserService.java
+++ src/main/java/com/example/drama_kill_system/service/IUserService.java
@@ -16,5 +16,4 @@ public interface IUserService extends IService<User> {
 
     Boolean opLogin(String password);
 
-
 }
--- src/test/java/com/example/drama_kill_system/DramaKillSystemApplicationTests.java
+++ src/test/java/com/example/drama_kill_system/DramaKillSystemApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.drama_kill_system;
 
+import com.example.drama_kill_system.mapper.ApplicationMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DramaKillSystemApplicationTests {
 
+    @Autowired
+    private ApplicationMapper applicationMapper;
+
+
     @Test
     void contextLoads() {
+        System.out.println(applicationMapper.selectById(1));
+        System.out.println("哈哈哈哈");
     }
 
 }
